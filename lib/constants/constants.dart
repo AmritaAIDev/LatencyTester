101fb254d011498909867d71214f70ce36363b09
Abhishek <abhishekkumar@amritaelearning.org>
2025-07-04 15:41:29 +0530

    feat:added constants file

---
+++ lib/constants/constants.dart
@@ -0,0 +1,14 @@
+class Constants {
+  static const BASE_URL = 'https://api.openai.com';
+  static const REGISTER = '$BASE_URL/api/create/';
+  static const CHAT = '$BASE_URL/v1/chat/completions';
+  static const CHAT_COLLECTION = '$BASE_URL/api/chat-storage/';
+
+  //for Bhasini Translation API
+  static const String authorisationKey = '8b5kbfrMHU1VlQlc4XMVugdqNig8lMS3Q-y0K1C7e68w0OvCEEV9pRQ_HlAi2HDt';
+  static const BHASINI_URL = 'https://dhruva-api.bhashini.gov.in/services/inference/pipeline';
+
+
+
+}
+
